08d5622ef410c576181859120011030bad0e4fcf
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-17 08:15:43 -0800

    Context Practice

--- week1/context-practice/src/components/Main.js
+++ week1/context-practice/src/components/Main.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {ThemeContextConsumer} from "../apps/themeContext"
 
-class Main extends Component {
-    render() {
-        return (
-            <ThemeContextConsumer>
-                {context => (
-                    <main className={`${context.theme}-theme`}>
-                        <div style={{textAlign: 'center', padding: '20px'}}>
-                            <h2>Click the button to toggle the Dark theme!</h2>
-                            <button onClick={context.toggleTheme}>Toggle Theme</button>
-                        </div>
-                    </main>
-                )}
-            </ThemeContextConsumer>
-        )
-    }
+function Main() {
+    return (
+        <ThemeContextConsumer>
+            {context => (
+                <main className={`${context.theme}-theme`}>
+                    <div style={{textAlign: 'center', padding: '20px'}}>
+                        <h2>Click the button to toggle the Dark theme!</h2>
+                        <button onClick={context.toggleTheme}>Toggle Theme</button>
+                    </div>
+                </main>
+            )}
+        </ThemeContextConsumer>
+    )
 }
 export default Main
--- week1/context-practice/src/index.js
+++ week1/context-practice/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import ThemeContext from './apps/themeContext'
 import {ThemeContextProvider} from "./apps/themeContext"
 import App from './apps/App'
 
