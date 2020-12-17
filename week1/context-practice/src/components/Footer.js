e2c2a0c1654e3fc991e5dbcb1bd0cf14143c145d
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-16 21:21:50 -0800

    Context Practice

--- week1/context-practice/src/components/Footer.js
+++ week1/context-practice/src/components/Footer.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 //import ThemeContext from '../apps/themeContext'
-import {ThemeContextConsumer} from "../apps/themeContext"
+import {ThemeContextConsumer, ThemeContextProvider} from "../apps/themeContext"
 
-function Footer() {
-    return (
+class Footer extends Component {
 
+    //static contextType = ThemeContextConsumer
+
+    render() {
+        return(
+            <footer className={`${this.context}-theme`}>
+                <div style={{textAlign: 'center', padding: '20px'}}>
+                    <span>The amazing Footer</span>
+                </div>
+            </footer>
+        )
+    }
+
+}
+
+Footer.contextType = ThemeContextConsumer
+
+//function Footer() {
+    //return (
+
+        /*
         <ThemeContextConsumer>
             {context => (
                 <footer className={`${context.theme}-theme`}>
@@ -14,6 +33,7 @@ function Footer() {
                 </footer>
             )}
         </ThemeContextConsumer>
+        */
 
         /*
         <ThemeContext.Consumer>
@@ -26,7 +46,7 @@ function Footer() {
             )}
         </ThemeContext.Consumer>
         */
-    )
-}
+    //)
+//}
 
 export default Footer
