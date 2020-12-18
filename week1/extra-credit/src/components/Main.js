a0aba8d290aa0a6fe83b8edba942ad50bb3f4aad
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-18 08:00:38 -0800

    Extra Credit

--- week1/extra-credit/src/components/Main.js
+++ week1/extra-credit/src/components/Main.js
@@ -5,18 +5,20 @@ class Main extends Component {
     render() {
         return (
             <main className={`${this.props.context.theme}-theme`}>
-                <div className='common'>
-                    <h2>Select a theme from the menu!</h2>
-                    <select 
-                        value={this.props.context.theme} 
-                        onChange={this.props.context.selectTheme}>
-                        <option value='dark'>Dark</option>
-                        <option value='light'>Light</option>
-                        <option value='red'>Red</option>
-                        <option value='pink'>Pink</option>
-                        <option value='purple'>Purple</option>
-                        <option value='blue'>Blue</option>
-                    </select>
+                <div>
+                    <div className='common'>
+                        <h2>Select a theme from the menu!</h2>
+                        <select 
+                            value={this.props.context.theme} 
+                            onChange={this.props.context.selectTheme}>
+                            <option value='dark'>Dark</option>
+                            <option value='light'>Light</option>
+                            <option value='red'>Red</option>
+                            <option value='pink'>Pink</option>
+                            <option value='purple'>Purple</option>
+                            <option value='blue'>Blue</option>
+                        </select>
+                    </div>
                 </div>
             </main>
         )
