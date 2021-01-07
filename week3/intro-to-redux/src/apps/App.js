514af5ec0ef70cddd45a3c06ca3c26e1cac57c9e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-01-06 21:40:08 -0800

    Intro to Redux

--- week3/intro-to-redux/src/apps/App.js
+++ week3/intro-to-redux/src/apps/App.js
@@ -1,8 +1,57 @@
 import React from 'react'
 
 function App() {
+    const redux = require("redux")
+    const state = {
+        count: 0
+    }
+
+    function increment() {
+        return {
+            type: 'INCREMENT'
+        }
+    }
+
+    function decrement() {
+        return {
+            type: 'DECREMENT'
+        }
+    }
+
+    function reducer(prevState = state, action) {
+        switch(action.type) {
+            case "INCREMENT":
+                return {
+                    count: prevState.count + 1
+                }
+            case "DECREMENT":
+                return {
+                    count: prevState.count - 1
+                }
+            default:
+                return prevState
+        }
+    }
+
+    const store = redux.createStore(reducer)
+
+    store.subscribe(() => {
+        console.log(store.getState())
+    })
+
     return (
-        <div>React with Redux!</div>
+        <div>
+            <button onClick={() => {
+                store.dispatch(increment())
+            }}>
+                Increment
+            </button>
+            <button onClick={() => {
+                store.dispatch(decrement())
+            }}>
+                Decrement
+            </button>
+        </div>
     )
 }
 
---
+++ week3/intro-to-redux/src/apps/components/Timer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Timer(props) {
+    return (
+        <div></div>
+
+    )
+}
+
+export default Timer
