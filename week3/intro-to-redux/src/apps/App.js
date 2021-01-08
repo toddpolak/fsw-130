6820511dd4c15e529c491287f7547ef78330c1a8
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-01-07 23:26:48 -0800

    Intro to Redux

--- week3/intro-to-redux/src/apps/App.js
+++ week3/intro-to-redux/src/apps/App.js
@@ -28,41 +28,18 @@ class App extends Component {
         }
     }
 
-    /*
-    timeToString(time) {
-        let diffInHrs = time / 3600000;
-        let hh = Math.floor(diffInHrs);
-      
-        let diffInMin = (diffInHrs - hh) * 60;
-        let mm = Math.floor(diffInMin);
-      
-        let diffInSec = (diffInMin - mm) * 60;
-        let ss = Math.floor(diffInSec);
-      
-        let diffInMs = (diffInSec - ss) * 1000;
-        let ms = Math.floor(diffInMs);
-      
-        let formattedMM = mm.toString().padStart(2, "0");
-        let formattedSS = ss.toString().padStart(2, "0");
-        let formattedMS = ms.toString().padStart(2, "0");
-        return `${formattedMM}:${formattedSS}${formattedMS}`;
-    }
-    */
-
-    startTest(){
-        let startTime = Date.now();
-        setInterval(function printTime(){
-            let elapsedTime = Date.now() - startTime;
-            //console.log(this.timeToString(elapsedTime))
-        }, 0)
+    startCounter() {
+        setInterval(() => {
+            store.dispatch({type: 'START'})
+        }, 100)
     }
 
     render() {
         return (
             <div>
                 <button onClick={() => {
-                    store.dispatch(this.start())
-                    this.startTest()
+                    this.startCounter()
+                    //store.dispatch(this.start())
                 }}>
                     Start
                 </button>
